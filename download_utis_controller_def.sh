368b51860a52d9e4984bcdf6f621eb6ab8f20f06
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2015-11-02 15:10:14 +0100

    client api generation working

--- download_utis_controller_def.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s -o src/main/resources/utis-controller.json http://cybertaxonomy.eu/eu-bon/utis/api-docs/default/utis-controller.json
--- generate-client-code.sh
+++ generate-client-code.sh
@@ -2,21 +2,28 @@
 
 LIB=target/lib
 
-SPEC_FILE=http://cybertaxonomy.eu/eu-bon/utis/api-docs/default/utis-controller.json
+#SPEC_FILE=http://cybertaxonomy.eu/eu-bon/utis/api-docs/default/utis-controller.json
+SPEC_FILE=http://127.0.0.1:8080/eubon-utis/api-docs/default/utis-controller.json
+
+CODEGEN_CONF=./src/main/resources/swagger-codegen-conf.json
 
 echo "starting client creation"
 mvn clean dependency:copy
 java -jar $LIB/swagger-codegen-cli.jar config-help -l java
 
-# curl -s -o src/main/resources/utis-controller.json http://cybertaxonomy.eu/eu-bon/utis/api-docs/default/utis-controller.json
+# curl -s -o src/main/resources/utis-controller.json $SPEC_FILE
 
 echo "generating client sources"
 java -jar $LIB/swagger-codegen-cli.jar generate \
-  -i http://cybertaxonomy.eu/eu-bon/utis/api-docs/default/utis-controller.json -l java \
+  -i $SPEC_FILE -l java \
   -o ./target/client/src/java/ \
-  -c ./src/main/resources/swagger-codegen-conf.json
+  -c $CODEGEN_CONF
   
 echo "building client project"
 
 cd ./target/client/src/java/
 mvn install
+
+# the compilation failes due to the class Classification with getthe getter getClass() 
+# this is another reason to refactor the Classification in the tnrMessage
+#
